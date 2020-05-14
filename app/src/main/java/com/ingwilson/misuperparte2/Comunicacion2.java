206878b48181147d5c880ae7c890f502d422a8d5
Wilson Callisaya <demarktec@gmail.com>
2020-05-14 17:53:45 -0500

    test

--- app/src/main/java/com/ingwilson/misuperparte2/Comunicacion2.java
+++ app/src/main/java/com/ingwilson/misuperparte2/Comunicacion2.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+
 public class Comunicacion2  extends AppCompatActivity {
     TextView txtmensaje;
     String nombre;
